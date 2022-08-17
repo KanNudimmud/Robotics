b37916bf2b90f07d5e78b1d9df06a466b38cbae7
Eray Mutlu <75501882+KanNudimmud@users.noreply.github.com>
2022-08-17 15:10:51 +0300

    Add files via upload

---
+++ Localization.py
@@ -0,0 +1,360 @@
+## Modify the empty list, p, so that it becomes a UNIFORM probability
+# distribution over five grid cells, as expressed in a list of
+# five probabilities.
+
+p = [0.2,0.2,0.2,0.2,0.2]
+
+print p   # TRUE
+
+## Modify your code to create probability vectors, p, of arbitrary
+# size, n. Use n=5 to verify that your new solution matches
+# the previous one.
+
+p=[]
+n=5
+
+print p  # [0.2,0.2,0.2,0.2,0.2] should be
+
+## Write code that outputs p after multiplying each entry
+#by pHit or pMiss at the appropriate places. Remember that
+#the red cells 1 and 2 are hits and the other green cells
+#are misses.
+
+
+p=[0.2,0.2,0.2,0.2,0.2]
+pHit = 0.6
+pMiss = 0.2
+
+#Enter code here
+p = []
+
+print p
+
+## Modify the program to find and print the sum of all the entries in the list p.
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+pHit = 0.6
+pMiss = 0.2
+
+p[0]=p[0]*pMiss
+p[1]=p[1]*pHit
+p[2]=p[2]*pHit
+p[3]=p[3]*pMiss
+p[4]=p[4]*pMiss
+
+# Enter your code below
+print(sum(p)) # TRUE
+
+## Modify the code below so that the function sense, which
+#takes p and Z as inputs, will output the NON-normalized
+#probability distribution, q, after multiplying the entries
+#in p by pHit or pMiss according to the color in the
+#corresponding cell in world.
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    return q
+
+print sense(p,Z) # TRUE
+
+## Modify your code so that it normalizes the output for
+#the function sense. This means that the entries in q
+#should sum to one.
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(p)):
+        q[i]=q[i]/s
+    return q
+print sense(p,Z) # TRUE
+
+## Try using your code with a measurement of 'green' and
+#make sure the resulting probability distribution is correct.
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'green'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(p)):
+        q[i]=q[i]/s
+    return q
+print sense(p, Z) # TRUE
+
+#Modify the code so that it updates the probability twice
+#and gives the posterior distribution after both
+#measurements are incorporated. Make sure that your code
+#allows for any sequence of measurement of any length.
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+#
+#ADD YOUR CODE HERE
+#
+print p
+
+## Program a function that returns a new distribution
+#q, shifted to the right by U units. If U=0, q should
+#be the same as p.
+
+p=[0, 1, 0, 0, 0]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+def move(p, U):
+    #
+    U = U % len(p)
+    q = p[-U:] + p[:-U]
+    #
+    return q
+
+print move(p, 1) # TRUE
+
+
+## Modify the move function to accommodate the added
+# probabilities of overshooting or undershooting
+# the intended destination.
+
+p = [0, 1, 0, 0, 0]
+world = ['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
+
+
+def sense(p, Z):
+    q = []
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1 - hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
+    return q
+
+print move(p, 1) # TRUE
+
+
+## Write code that makes the robot move twice and then prints
+# out the resulting distribution, starting with the initial
+# distribution p = [0, 1, 0, 0, 0]
+
+p=[0, 1, 0, 0, 0]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
+    return q
+#
+p = move(p,1)
+p = move(p,1)
+print p # TRUE
+
+
+## Write code that moves 1000 times and then prints the
+#resulting probability distribution.
+
+p=[0, 1, 0, 0, 0]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
+    return q
+#
+for i in range(100):
+    p = move(p,1)
+#
+print p # TRUE
+
+
+## Given the list motions=[1,1] which means the robot
+#moves right and then right again, compute the posterior
+#distribution if the robot first senses red, then moves
+#right one, then senses green, then moves right again,
+#starting with a uniform prior distribution.
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+motions = [1,1]
+pHit = 0.6
+pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
+    return q
+#
+# ADD CODE HERE
+p = sense(p,"red")
+p = move(p,1)
+p = sense(p,"green")
+p = move(p,1)
+#
+print p # TRUE
+
+# Modify the previous code so that the robot senses red twice.
+
+p = [0.2, 0.2, 0.2, 0.2, 0.2]
+world = ['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+motions = [1, 1]
+pHit = 0.6
+pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
+
+def sense(p, Z):
+    q = []
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1 - hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i - U) % len(p)]
+        s = s + pOvershoot * p[(i - U - 1) % len(p)]
+        s = s + pUndershoot * p[(i - U + 1) % len(p)]
+        q.append(s)
+    return q
+
+for k in range(len(measurements)):
+    p = sense(p, measurements[0])
+    p = move(p, motions[k])
+
+print p  # TRUE
+
+
+
+
+
+
